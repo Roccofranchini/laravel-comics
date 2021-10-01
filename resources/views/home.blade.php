6820a91a34d74ebf941d466e6ae384893da0561d
Roccofranchini <rocco.franchini1@gmail.com>
2021-10-01 16:44:19 +0200

    X

---
+++ resources/views/home.blade.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <header id="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-6">
+					<img src="{{ asset('img/dc-logo.png') }}" alt="" />
+				</div>
+				<div class="col-6 d-flex align-items-center">
+					<ul class="mb-0">
+						<li><a href="">CHARACTERS</a></li>
+                        <li><a href=""  class="active">COMICS</a></li>
+                        <li><a href="">MOVIES</a></li>
+                        <li><a href="">TV</a></li>
+                        <li><a href="">GAMES</a></li>
+                        <li><a href="">COOLECTIBLES</a></li>
+                        <li><a href="">VIDEOS</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</header>
+    <main>
+        <section id="content">
+		    <section id="cartoon">
+			    <span class="current-series">CURRENT SERIES</span>
+		    </section>
+		    <div class="container">
+			    <div class="row">
+				    <p>a</p>
+			    </div>
+		    </div>
+	    </section>
+    </main>
+    <footer>
+    <section id="links">
+		<div class="container">
+			<div class="row">
+				<div class="col-6">
+					<div class="col">
+						<ul>
+							<h4>DC GAMES</h4>
+							<li>Characters</li>
+							<li>Comics</li>
+							<li>Movies</li>
+							<li>TV</li>
+							<li>Games</li>
+							<li>Videos</li>
+							<li>News</li>
+						</ul>
+						<ul>
+							<h4>SHOP</h4>
+							<li>Shop DC</li>
+							<li>Shop DC Collectibles</li>
+						</ul>
+					</div>
+					<div class="col">
+						<ul>
+							<h4>DC</h4>
+							<li>Terms of use</li>
+							<li>Privacy policy</li>
+							<li>Ad choiches</li>
+							<li>Advertising</li>
+							<li>Jobs</li>
+							<li>Subscription</li>
+							<li>Talent Workshops</li>
+							<li>CPSC Certificates</li>
+							<li>Ratings</li>
+							<li>Shop Help</li>
+							<li>Contact Us</li>
+						</ul>
+					</div>
+					<div class="col">
+						<ul>
+							<h4>SITES</h4>
+							<li>DC</li>
+							<li>MAD Magazine</li>
+							<li>DC kids</li>
+							<li>DC Universe</li>
+							<li>DC Power Visa</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-6">
+					<div class="img-cont">
+						<img src="{{ asset('img/dc-logo-bg.png') }}" alt="" />
+                        
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+		<div class="container">
+			<div class="row">
+				<div class="sing-up">
+					<a href="">SING-UP NOW!</a>
+				</div>
+				<div>
+					<div class="social row">
+						<a href=""> FOLLOW US</a>
+						<img src="../assets/img/footer-facebook.png" alt="" />
+						<img src="../assets/img/footer-twitter.png" alt="" />
+						<img src="../assets/img/footer-youtube.png" alt="" />
+						<img src="../assets/img/footer-pinterest.png" alt="" />
+						<img src="../assets/img/footer-periscope.png" alt="" />
+						<img src="" alt="" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+    <script src="asset('js/app.js')"></script>
+</body>
+</html>
